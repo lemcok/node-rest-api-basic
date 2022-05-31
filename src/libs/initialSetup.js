831ff6a05c7371dd99f77749d46b6b07346e3b61
Ricardo Flores <rik.fm1809@gmail.com>
2022-05-31 17:01:53 -0500

    add User Admin to the InitialSetup

--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -1,11 +1,13 @@
 import Role from '../models/Role';
+import User from '../models/User';
 
 export const createRoles = async () => {
 
     try {
         const count = await Role.estimatedDocumentCount();
+        const countUsers = await User.estimatedDocumentCount();
 
-        if (count>0) return;
+        if (count>0 && countUsers > 0) return;
 
         const values = await Promise.all([
             new Role({name: 'admin'}).save(),
@@ -13,6 +15,18 @@ export const createRoles = async () => {
             new Role({name: 'user'}).save()
         ])
         console.log(values);
+
+        const foundRole = await Role.find({name: "admin"})
+        const valuesUser = await Promise.all([
+            new User({
+                "name": "admin",
+                "email": "admin@example.com",
+                "password": await User.encryptPassword("admin"),
+                "image": "null",
+                "roles": [foundRole[0]._id]
+            }).save()
+        ])
+        console.log( valuesUser )
     } catch (error) {
         console.log(error)
     }
